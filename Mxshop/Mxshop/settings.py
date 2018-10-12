941df95f5a724e81c310f29305aa8624034e5611
DYQDYQ <1101546384@qq.com>
2018-10-12 18:12:41 +0800

    sa

--- Mxshop/Mxshop/settings.py
+++ Mxshop/Mxshop/settings.py
@@ -31,6 +31,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL = 'nusers.UserProfile'
+
 
 # Application definition
 
---
+++ Mxshop/apps/nusers/models.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from datatime import datatime
+
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+
+
+class UserProfile(AbstractUser):
+    """用户"""
+    name = models.CharField(max_length=30, null=True, blank=True,verbose_name="姓名")
+    birthday = models.CharField(null=True, blank=True,verbose_name="出生年月")
+    gender = models.CharField(max_length=6, choices=(("male",u"男"), ('female', u"女")),default="male", verbose_name="性别")
+    mobile = models.CharField(max_length=11, verbose_name="电话")
+    email = models.EmailField(max_length=100, null=True, blank=True, verbose_name="邮箱")
+
+    class Meta:
+        verbose_name = "用户"
+        verbose_name_plural = "用户"
+
+    def __str__(self):
+        return self.name
+
+
+class VerifyCode(models.Model):
+    """短信验证码"""
+    code = models.CharField(max_length=10, verbose_name="验证码")
+    mobile = models.CharField(max_length=11, verbose_name="电话")
+    add_time = models.DateTimeField(default=datatime.now, verbose_name="添加时间")
+
+    class Meta:
+        verbose_name="短信验证码"
+        verbose_name_plural=verbose_name
+
+    def __str__(self)
+        return self.code
+
+                
+
